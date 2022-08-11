cabc46c4ef38721e41003e0b1f198a84ac5f23c5
vlad <vladbelyvskij@gmail.com>
2022-08-11 18:47:29 +0300

    Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.

---
+++ task001HW2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (number > 100)
+{
+    number /= 10;
+    result = number % 10;
+}
+Console.Write("Вторая цифра числа: " + result);
